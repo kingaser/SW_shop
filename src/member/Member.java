f15e5acd1096677f62fb948fd7db3f12abef42dd
HyeGwang Kang <kingaser@naver.com>
2023-06-27 16:00:28 +0900

    feat : 회원 엔티티 작성

---
+++ src/member/Member.java
@@ -0,0 +1,43 @@
+package member;
+
+import java.util.Scanner;
+
+// TODO: 2023-06-27 회원 엔티티
+public class Member {
+
+    Scanner kb = new Scanner(System.in);
+//    private int id;
+    private String name;
+    private String nickName;
+    private String phoneNumber;
+    private String address;
+
+    public Member() {};
+
+    public Member(String name, String nickName, String phoneNumber, String address) {
+//        this.id = id;
+        this.name = name;
+        this.nickName = nickName;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getPhoneNumber() {
+        return this.phoneNumber;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+//    public int getId() {
+//        return this.id;
+//    }
+}
