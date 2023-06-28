0e415023bb3cf985cdfabfecb13852e8678f4291
HyeGwang Kang <kingaser@naver.com>
2023-06-28 11:28:58 +0900

    feat : Basket 엔티티 회원 이름 필드 추가

--- src/shop/basket/Basket.java
+++ src/shop/basket/Basket.java
@@ -1,13 +1,19 @@
-package shop;
+package shop.basket;
 
 public class Basket {
 
+    private String userName;
     private String itemName;
     private int itemPrice;
 
-    public Basket(String itemName, int itemPrice) {
+    public Basket(String userName, String itemName, int itemPrice) {
+        this.userName = userName;
         this.itemName = itemName;
         this.itemPrice = itemPrice;
+    }
+
+    public String getUserName() {
+        return userName;
     }
 
     public String getItemName() {
