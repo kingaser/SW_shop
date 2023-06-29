4bb3588daa669a79f1aba581ef6d557c1fadc2eb
HyeGwang Kang <kingaser@naver.com>
2023-06-29 17:07:13 +0900

    feat : id 필드 추가

--- src/shop/ShoppingList.java
+++ src/shop/ShoppingList.java
@@ -2,14 +2,24 @@ package shop;
 
 public class ShoppingList {
 
+    private int id;
     private String itemName;
     private int itemPrice;
     private int quantity;
 
-    public ShoppingList(String itemName, int itemPrice, int quantity) {
+    public ShoppingList(int id, String itemName, int itemPrice, int quantity) {
+        this.id = id;
         this.itemName = itemName;
         this.itemPrice = itemPrice;
         this.quantity = quantity;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getItemName() {
