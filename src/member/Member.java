883a0ff98bf9e1703bdc1d5da7dd436c70bbbff6
HyeGwang Kang <kingaser@naver.com>
2023-06-29 17:07:55 +0900

    feat : Member class id 필드 추가

--- src/member/Member.java
+++ src/member/Member.java
@@ -3,17 +3,26 @@ package member;
 // TODO: 2023-06-27 회원 엔티티
 public class Member {
 
-//    private int id;
+    private int id;
     private String name;
     private String nickName;
     private String phoneNumber;
     private String address;
 
-    public Member(String name, String nickName, String phoneNumber, String address) {
+    public Member(int id, String name, String nickName, String phoneNumber, String address) {
+        this.id = id;
         this.name = name;
         this.nickName = nickName;
         this.phoneNumber = phoneNumber;
         this.address = address;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getName() {
