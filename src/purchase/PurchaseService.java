7edbf78a6a8951543fa55ce7dc7513621357ba27
HyeGwang Kang <kingaser@naver.com>
2023-07-03 09:09:42 +0900

    feat : 구매 목록 보기 로직 수정

--- src/purchase/PurchaseService.java
+++ src/purchase/PurchaseService.java
@@ -4,7 +4,6 @@ import adminitem.AdminItem;
 import adminitem.AdminItemService;
 import member.Member;
 import member.MemberService;
-import basket.BasketService;
 
 import java.io.*;
 import java.util.Collections;
@@ -25,7 +24,6 @@ public class PurchaseService extends AdminItemService {
     );
 
     MemberService memberService = new MemberService();
-    BasketService basketService = new BasketService();
 
     // TODO: 2023-06-30 멤버 변수 설정
     private LinkedHashMap<Integer, Purchase> purchaseList = new LinkedHashMap<>();
@@ -56,24 +54,10 @@ public class PurchaseService extends AdminItemService {
                 System.out.println("회원 이름을 입력해주세요.");
                 String userName = kb.next();
                 findAllItem();
-                System.out.println("0. 이전 메뉴(진행 내용 저장)\n1. 구매\n2. 장바구니에 담기");
-                int menuNum = kb.nextInt();
-                while (true) {
-                    if (menuNum == 0) {
-                        break;
-                    } else if (menuNum == 1) {
-                        System.out.println("구매하실 상품의 이름과 개수를 입력해주세요.");
-                        String itemName = kb.next();
-                        int itemCount = kb.nextInt();
-                        addPurchase(userName, itemName, itemCount);
-                        break;
-                    } else if (menuNum == 2) {
-                        System.out.println("장바구니에 담을 상품의 이름을 입력해 주세요");
-                        String itemName = kb.next();
-                        basketService.addBasket(userName, itemName);
-                        break;
-                    }
-                }
+                System.out.println("구매하실 상품의 이름과 개수를 입력해주세요.");
+                String itemName = kb.next();
+                int itemCount = kb.nextInt();
+                addPurchase(userName, itemName, itemCount);
             } else {
                 System.out.println("잘못된 입력입니다.\n다시 입력해주세요.");
             }
@@ -105,6 +89,7 @@ public class PurchaseService extends AdminItemService {
                 , adminItem.getItemPrice(), itemCount);
         if (!purchaseList.isEmpty()) {
             purchaseId = Collections.max(purchaseList.keySet()) + 1;
+            purchase.setId(purchaseId);
             purchaseList.put(purchaseId, purchase);
         } else {
             purchaseList.put(purchaseId, purchase);
@@ -115,6 +100,24 @@ public class PurchaseService extends AdminItemService {
         adminItemList.put(adminItem.getId(), adminItem);
     }
 
+    public void findAllPurchase(String userName) {
+        Member member = memberService.checkMember(userName);
+        System.out.println(member.getName() + "님의 구매목록!!");
+
+        StringBuilder sb = new StringBuilder();
+        Iterator<Purchase> purchaseIterator = purchaseList.values().iterator();
+        while (purchaseIterator.hasNext()) {
+            Purchase purchase = purchaseIterator.next();
+            if (purchase.getUserName().equals(userName)) {
+                sb.append(purchase.getId()).append(" ")
+                        .append(purchase.getUserName()).append(" ")
+                        .append(purchase.getItemName()).append(" ")
+                        .append(purchase.getQuantity()).append("\n");
+            }
+        }
+        System.out.println(sb);
+    }
+
     // TODO: 2023-06-28 읽어온 파일 Map에 저장
     private void readPurchaseInputCheck() {
         try {
@@ -123,15 +126,15 @@ public class PurchaseService extends AdminItemService {
                 String[] tmp = purchaseTmp.split(" ");
                 int id = Integer.parseInt(tmp[0]);
                 purchaseId = id;
-                String userName = tmp[0];
-                String itemName = tmp[1];
-                int itemPrice = Integer.parseInt(tmp[2]);
-                int quantity = Integer.parseInt(tmp[3]);
+                String userName = tmp[1];
+                String itemName = tmp[2];
+                int itemPrice = Integer.parseInt(tmp[3]);
+                int quantity = Integer.parseInt(tmp[4]);
                 Purchase purchase = new Purchase(id, userName, itemName, itemPrice, quantity);
                 purchaseList.put(purchase.getId(), purchase);
             }
         } catch (IOException e) {
-            System.out.println("회원 목록 조회 오류");
+            System.out.println("구매 목록 조회 오류");
             e.printStackTrace();
         }
     }
@@ -140,8 +143,11 @@ public class PurchaseService extends AdminItemService {
         Iterator<Purchase> memberIterator = purchaseList.values().iterator();
         while (memberIterator.hasNext()) {
             Purchase purchase = memberIterator.next();
-            sb.append(purchase.getId()).append(" ").append(purchase.getItemName()).append(" ")
-                    .append(purchase.getItemPrice()).append(" ").append(purchase.getQuantity()).append("\n");
+            sb.append(purchase.getId()).append(" ")
+                    .append(purchase.getUserName()).append(" ")
+                    .append(purchase.getItemName()).append(" ")
+                    .append(purchase.getItemPrice()).append(" ")
+                    .append(purchase.getQuantity()).append("\n");
         }
     }
 
@@ -150,7 +156,7 @@ public class PurchaseService extends AdminItemService {
         printPurchaseList(sb);
         try {
             new FileWriter(file).close();
-            purchaseListWriter.write(sb.toString());
+            purchaseListWriter.append(sb.toString());
             purchaseListWriter.flush();
             purchaseListWriter.close();
         } catch (IOException e) {
